83bcbc46309b808dab478837a6137158b7266ab6
nathan.f <nathan.flachaire.external@atos.net>
2023-11-23 16:43:40 +0100

    add no wrap to tree text good cursor this time

--- lua/custom/plugins/webmappings.lua
+++ lua/custom/plugins/webmappings.lua
@@ -7,7 +7,7 @@ return {
 
 			local r, c = unpack(vim.api.nvim_win_get_cursor(0))
 			vim.print(r)
-			c = c + 14
+			c = c + 13
 			vim.api.nvim_win_set_cursor(0, { r, c }) -- Set cursor position to 14th character (second quote)
 		end,
 		{ desc = 'Writes console.log' })
