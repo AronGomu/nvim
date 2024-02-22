40ec80d08293b3970a9b595934c7a4bef8044d52
Nathan FLACHAIRE <nathan.flachaire@sogelink.com>
2024-02-22 11:01:10 +0100

    remove auto preview cuz bad

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -31,18 +31,17 @@ return {
 						-- OR
 						require("neo-tree.command").execute({ action = "close" })
 					end
-				},
-
-				{
-					event = 'after_render',
-					handler = function()
-						local state = require('neo-tree.sources.manager').get_state('filesystem')
-						if not require('neo-tree.sources.common.preview').is_active() then
-							state.config = { use_float = false } -- or whatever your config is
-							state.commands.toggle_preview(state)
-						end
-					end
 				}
+				-- 	, {
+				-- 	event = 'after_render',
+				-- 	handler = function()
+				-- 		local state = require('neo-tree.sources.manager').get_state('filesystem')
+				-- 		if not require('neo-tree.sources.common.preview').is_active() then
+				-- 			state.config = { use_float = false } -- or whatever your config is
+				-- 			state.commands.toggle_preview(state)
+				-- 		end
+				-- 	end
+				-- }
 			},
 
 			close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
