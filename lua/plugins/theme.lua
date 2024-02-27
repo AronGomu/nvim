63ba20574dbccee477a6971c8ab2aa2cd371fb7c
Nathan FLACHAIRE <nathan.flachaire@sogelink.com>
2024-02-27 11:32:56 +0100

    doing random stuff with colorscheme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,23 +8,33 @@ return {
 	-- 	end,
 	-- },
 
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme 'tokyonight-moon'
-	-- 	end,
-	-- },
-
 	{
-		"Mofiqul/dracula.nvim",
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme 'dracula'
+			vim.cmd.colorscheme 'tokyonight-moon'
 		end,
-	}
+	},
+
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme 'dracula'
+	-- 	end,
+	-- },
+
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme 'slate'
+	-- 	end,
+	-- }
 
 
 
