6ec2c080f2a37eccd44328caabd8279641677577
Nathan FLACHAIRE <nathan.flachaire@sogelink.com>
2024-03-12 15:59:59 +0100

    adding asserts

--- lua/plugins/webmappings.lua
+++ lua/plugins/webmappings.lua
@@ -1,4 +1,7 @@
 return {
+
+
+	-- javascript throw error
 	vim.keymap.set("n", "<leader>wtne", function()
 			local pos = vim.api.nvim_win_get_cursor(0)[2]
 			local line = vim.api.nvim_get_current_line()
@@ -13,6 +16,7 @@ return {
 		{ desc = 'Writes throw new Error' }),
 
 
+	-- javascript console.log
 	vim.keymap.set("n", "<leader>wcl", function()
 			local pos = vim.api.nvim_win_get_cursor(0)[2]
 			local line = vim.api.nvim_get_current_line()
@@ -26,6 +30,36 @@ return {
 		end,
 		{ desc = 'Writes console.log' }),
 
+	-- javascript assert isObject
+	vim.keymap.set("n", "<leader>wasso", function()
+			local pos = vim.api.nvim_win_get_cursor(0)[2]
+			local line = vim.api.nvim_get_current_line()
+			local wass = 'assert(_.isObject(zzz), \"zzz should be an object !\\n\" + \"zzz :\\n" + JSON.stringify(zzz))'
+			local nline = line:sub(0, pos) .. wass .. line:sub(pos + 1)
+			vim.api.nvim_set_current_line(nline)
+
+			local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+			vim.print(r)
+			vim.api.nvim_win_set_cursor(0, { r, c }) -- Set cursor position to 14th character (second quote)
+		end,
+		{ desc = 'Writes throw new Error' }),
+
+	-- javascript assert isNumber
+	vim.keymap.set("n", "<leader>wassn", function()
+			local pos = vim.api.nvim_win_get_cursor(0)[2]
+			local line = vim.api.nvim_get_current_line()
+			local wass = 'assert(_.isNumber(zzz), \"zzz should be a number!\\n\" + \"zzz : \" + JSON.stringify(zzz))'
+			local nline = line:sub(0, pos) .. wass .. line:sub(pos + 1)
+			vim.api.nvim_set_current_line(nline)
+
+			local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+			vim.print(r)
+			vim.api.nvim_win_set_cursor(0, { r, c }) -- Set cursor position to 14th character (second quote)
+		end,
+		{ desc = 'Writes throw new Error' }),
+
+
+	-- coffee script console log
 	vim.keymap.set("n", "<leader>wccl", function()
 			local pos = vim.api.nvim_win_get_cursor(0)[2]
 			local line = vim.api.nvim_get_current_line()
